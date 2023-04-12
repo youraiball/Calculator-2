a2694ab58a0f8cfdfb58266071ddaccf85bd9a6b
Ai Tran <youraiball@gmail.com>
2023-04-12 06:58:19 -0700

    read input and write output txt files

--- calculator.py
+++ calculator.py
@@ -5,41 +5,55 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 ops_w_2 = ["+", "-", "*", "/", "pow", "mod"]
 quit_words = ["q", "quit", "Q", "Quit", "exit"]
-while True:
-    user_input = input("Enter your equation: ")
-    tokens = user_input.split()
 
-    if tokens[0] in quit_words:
-        print("Have a nice day.")
-        break
+file = open("test.txt")
+output_file = open("results.txt", "w")
+has_not_quit = True
+while has_not_quit:
+    for line in file: #user_input = input("Enter your equation: ")
+        print(line)
+        tokens = line.strip().split()
 
-    if len(tokens) < 2 and tokens[0] not in quit_words:
-        print("Incomplete input")
-        continue
-    elif len(tokens) == 2 or len(tokens) > 3:
-        if tokens[0] in ops_w_2:
-            print("This operation takes two numbers")
+        if tokens[0] in quit_words:
+            print("Have a nice day.")
+            has_not_quit = False
+            break
+
+        if len(tokens) < 2 and tokens[0] not in quit_words:
+            print("Incomplete input")
+            continue
+        elif len(tokens) == 2 or len(tokens) > 3:
+            if tokens[0] in ops_w_2:
+                print("This operation takes two numbers")
+                continue
+
+        if not tokens[1].isnumeric() or (tokens[0] in ops_w_2 and not tokens[2].isnumeric()):
+            print("Pls enter an integer")
             continue
 
-    if not tokens[1].isnumeric() or (tokens[0] in ops_w_2 and not tokens[2].isnumeric()):
-        print("Pls enter an integer")
-        continue
+        result = None
 
-    if tokens[0] == "+":
-        print(add(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == "-":
-        print(subtract(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == "*":
-        print(multiply(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == "/":
-        print(divide(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == "square":
-        print(square(float(tokens[1])))
-    elif tokens[0] == "cube":
-        print(cube(float(tokens[1])))
-    elif tokens[0] == "pow":
-        print(power(float(tokens[1]), float(tokens[2])))
-    elif tokens[0] == "mod":
-        print(mod(float(tokens[1]), float(tokens[2])))
-    else:
-        print("Invalid Operator. Pls try again.")
+        if tokens[0] == "+":
+            result = add(float(tokens[1]), float(tokens[2]))
+        elif tokens[0] == "-":
+            result = subtract(float(tokens[1]), float(tokens[2]))
+        elif tokens[0] == "*":
+            result = multiply(float(tokens[1]), float(tokens[2]))
+        elif tokens[0] == "/":
+            result = divide(float(tokens[1]), float(tokens[2]))
+        elif tokens[0] == "square":
+            result = square(float(tokens[1]))
+        elif tokens[0] == "cube":
+            result = cube(float(tokens[1]))
+        elif tokens[0] == "pow":
+            result = power(float(tokens[1]), float(tokens[2]))
+        elif tokens[0] == "mod":
+            result = mod(float(tokens[1]), float(tokens[2]))
+        else:
+            result = "Invalid Operator. Pls try again."
+
+        print(result)
+        output_file.write(str(result) + "\n")
+
+file.close()
+output_file.close()
