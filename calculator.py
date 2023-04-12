f998485421c273f9e05282e5b588e3aef43c5fc5
Ai Tran <youraiball@gmail.com>
2023-04-11 20:16:05 -0700

    handled edge cases

--- calculator.py
+++ calculator.py
@@ -3,21 +3,29 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
+ops_w_2 = ["+", "-", "*", "/", "pow", "mod"]
+quit_words = ["q", "quit", "Q", "Quit", "exit"]
 while True:
     user_input = input("Enter your equation: ")
     tokens = user_input.split()
 
-    if not tokens[1].isnumeric() or not tokens[2].isnumeric():
+    if tokens[0] in quit_words:
+        print("Have a nice day.")
+        break
+
+    if len(tokens) < 2 and tokens[0] not in quit_words:
+        print("Incomplete input")
+        continue
+    elif len(tokens) == 2 or len(tokens) > 3:
+        if tokens[0] in ops_w_2:
+            print("This operation takes two numbers")
+            continue
+
+    if not tokens[1].isnumeric() or (tokens[0] in ops_w_2 and not tokens[2].isnumeric()):
         print("Pls enter an integer")
         continue
 
-
-
-    if tokens[0] == "q":
-        print("Have a nice day.")
-        break
-    elif tokens[0] == "+":
+    if tokens[0] == "+":
         print(add(float(tokens[1]), float(tokens[2])))
     elif tokens[0] == "-":
         print(subtract(float(tokens[1]), float(tokens[2])))
@@ -33,3 +41,5 @@ while True:
         print(power(float(tokens[1]), float(tokens[2])))
     elif tokens[0] == "mod":
         print(mod(float(tokens[1]), float(tokens[2])))
+    else:
+        print("Invalid Operator. Pls try again.")
